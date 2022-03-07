97b546486f618023a1cbcbdd9852fa167cad9f71
Leandro Marim <60162892+Leandromarim@users.noreply.github.com>
2022-03-07 00:51:56 -0300

    Atualização Mobile dia 06/03/2022

---
+++ README.md
@@ -0,0 +1,4 @@
+#INTRANET PARTICULAR
+Essa INTRANET foi desenvolvida por: Leandro Marim, para o Hospital de Acidentados, por causa desse desenvolvimento
+subi de cargo, de Analista de TI para Coordenador de TI, mesmo assim estou buscando melhorias para deixar a INTRANET
+ainda melhor, para melhor usabilidade.
---
+++ assets/js/busca.js
@@ -0,0 +1,12 @@
+let titulo = document.queryselector(".titulo");
+let job = document.queryselector(".job");
+let form = document.queryselector(".form");
+
+form.addEventListener("submit", fuction(Event) {
+    Event.preventDefault();
+
+    let dados = {
+        titulo: name.value,
+        job: job.value
+    };
+})
